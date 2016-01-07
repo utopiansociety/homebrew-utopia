013d82dffc81d1a53dcaa46544e88ed43ef007b0
Avi W-H <wasbazi@gmail.com>
2016-01-07 13:35:46 -0700

    releasing version devel

--- utopia.rb
+++ utopia.rb
@@ -3,17 +3,17 @@ require 'formula'
 class Utopia < Formula
   homepage 'http://www.utopianapp.com'
 
-stable do
+  stable do
 	version '0.11.13'
 	@@file = "utopia_#{version}_darwin_amd64"
 	url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
 	sha1 '133e704ffc5b4ccfb5752b15e1c458dacb411988'
   end
 
-
+  
   devel do
-    url "https://s3.amazonaws.com/aetna-utopia/devel/utopia_#{version}_darwin_amd64.tar.gz"
-    sha1 '3890210885dd23a6b408dde052ae74aad992bab3'
+    url "https://s3.amazonaws.com/aetna-utopia/devel/utopia_devel_darwin_amd64.tar.gz"
+    sha1 '0ec5adceaa3a15c25e3945d12cee2a49748f78d6'
   end
 
   depends_on :arch => :intel
