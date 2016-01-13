c63236a09795ccc1e6849d2bd3625d0b76f9fb56
root <utopia@itriagehealth.com>
2016-01-13 20:51:24 +0000

    releasing version 0.11.14

--- utopia.rb
+++ utopia.rb
@@ -3,14 +3,14 @@ require 'formula'
 class Utopia < Formula
   homepage 'http://www.utopianapp.com'
 
+  
   stable do
-	version '0.11.13'
-	@@file = "utopia_#{version}_darwin_amd64"
-	url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
-	sha1 '133e704ffc5b4ccfb5752b15e1c458dacb411988'
+    version '0.11.14'
+    @@file = "utopia_#{version}_darwin_amd64"
+    url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
+    sha1 '968813e76e46ebc002a67728b558374444128219'
   end
 
-  
   devel do
     version 'devel-01132016-202650'
     url "https://s3.amazonaws.com/aetna-utopia/devel/utopia_#{version}_darwin_amd64.tar.gz"
