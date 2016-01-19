89f0f5ce81b01280c2b14e5b77f0c64268e62d67
root <utopia@itriagehealth.com>
2016-01-19 18:27:52 +0000

    releasing version devel-01192016-182551

--- utopia.rb
+++ utopia.rb
@@ -12,9 +12,9 @@ class Utopia < Formula
 
   
   devel do
-    version 'devel-01192016-160324'
+    version 'devel-01192016-182551'
     url "https://s3.amazonaws.com/aetna-utopia/devel/utopia_#{version}_darwin_amd64.tar.gz"
-    sha1 'b0cf0d31446e235c13e4ca6ded68a0d68ce34218'
+    sha1 '9beb388cfc50233e9b336b3f9e9ccda7aee7926e'
   end
 
   depends_on :arch => :intel
